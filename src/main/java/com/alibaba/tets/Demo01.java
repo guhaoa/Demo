efcf756c63859003439fa0e0fcb2d5574cc1f960
ritian <7686212@qq.com>
2017-08-03 23:20:05 +0800

    主分支提交

--- src/main/java/com/alibaba/tets/Demo01.java
+++ src/main/java/com/alibaba/tets/Demo01.java
@@ -29,7 +29,7 @@ public class Demo01 {
         System.out.println(list.toString());*/
         repeatMessage("你好啊",1000);
 
-
+        System.out.println("我是主分支啊");
         System.out.println("你好好吗");
     }
 
