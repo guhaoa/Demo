29c9c7ad1f1d15ee803ce7f6fcc17ce50c847508
ritian <7686212@qq.com>
2017-08-12 12:30:15 +0800

    'addFeatureTest'

---
+++ src/main/java/com/alibaba/tets/FeatureTest.java
@@ -0,0 +1,14 @@
+package com.alibaba.tets;
+
+import lombok.Data;
+
+/**
+ * @author MSI
+ * @date 2017/08/12
+ */
+@Data
+public class FeatureTest {
+    private String name;
+    private int age;
+
+}
