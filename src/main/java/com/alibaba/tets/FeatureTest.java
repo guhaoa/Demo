d0875b9f6743ef05ff237750d71a75275967b85c
ritian <7686212@qq.com>
2017-08-12 12:50:55 +0800

    'featureCommitTest'

--- src/main/java/com/alibaba/tets/FeatureTest.java
+++ src/main/java/com/alibaba/tets/FeatureTest.java
@@ -6,9 +6,24 @@ import lombok.Data;
  * @author MSI
  * @date 2017/08/12
  */
-@Data
+
 public class FeatureTest {
     private String name;
     private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
